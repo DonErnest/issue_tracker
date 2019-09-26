727756fcdef8310baca84fa72b5b1dfdaf7bfc8f
DonErnest <valeetmihifaveas@gmail.com>
2019-09-26 19:15:35 +0600

    создание моделей Task, Type и Status, регистрация в панели админа, откат первоначальной миграции, добавление Meta к модели Status

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task, Type, Status
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'type','created_at']
+    list_display_links = ['id', 'summary']
+    list_filter = ['type', 'status']
+    search_fields = ['summary', 'description']
+    fields = ['summary', 'description', 'status', 'type', 'created_at']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    summary = models.CharField(max_length=200, null=False, blank=False, verbose_name='summary')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='description')
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT,related_name='tasks', verbose_name='status')
+    type = models.ForeignKey('webapp.Type',on_delete=models.PROTECT, related_name='tasks', verbose_name='type')
+    created_at= models.DateTimeField(auto_now_add=True, verbose_name='created')
+
+    def __str__(self):
+        return self.summary
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=15, verbose_name='task_name')
+
+    def __str__(self):
+        return self.name
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=15, verbose_name='status_name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='Status'
+        verbose_name_plural='Status'
