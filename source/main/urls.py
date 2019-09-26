f540201b3fd2ed9d8eb4560b507d9bf246bdff6d
DonErnest <valeetmihifaveas@gmail.com>
2019-09-26 20:01:13 +0600

    создание страницы детального просмотра task.html, настройка роутера

--- source/main/urls.py
+++ source/main/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import IndexView
+from webapp.views import IndexView, TaskView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(),name='main_page')
+    path('', IndexView.as_view(),name='main_page'),
+    path('task/<int:pk>', TaskView.as_view(), name='task_view'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 
 from webapp.models import Task
@@ -13,3 +13,12 @@ class IndexView(TemplateView):
         context['tasks'] = Task.objects.all()
         return context
 
+class TaskView(TemplateView):
+
+    template_name = 'task.html'
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get('pk')
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=pk)
+        return context
