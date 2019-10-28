fa1735bd7e5690e0b5c7a769674fdf49e790b079
DonErnest <valeetmihifaveas@gmail.com>
2019-10-28 15:58:56 +0600

    Налажена работа clean, рефакторинг кода в forms.py, исправление вывода ошибок в шаблоне регистрации, обновление фикстур

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -3,25 +3,38 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 
-class SignUpForm(forms.Form):
-    username=forms.CharField(max_length=100, required=True, label='Username')
-    password=forms.CharField(max_length=100, required=True, label='Password', widget=forms.PasswordInput)
-    password_confirm = forms.CharField(max_length=100, required=True, label='Password input', widget=forms.PasswordInput)
+class UserSignUpForm(forms.ModelForm):
+    email = forms.EmailField(label='Электронный адрес', required=True, widget=forms.EmailInput)
+    password=forms.CharField(max_length=100, required=True, label='Password', widget=forms.PasswordInput, strip=False)
+    password_confirm = forms.CharField(max_length=100, required=True, label='Password input', widget=forms.PasswordInput, strip=False)
 
-
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        try:
-            User.objects.get(username=username)
-            raise ValidationError('Username already exists!', code='username_already_occupied')
-        except User.DoesNotExist:
-            return username
-
-    def clean(self):
-        super(SignUpForm, self).clean()
+    def password_check(self):
         password = self.cleaned_data.get('password')
         password_check = self.cleaned_data.get('password_confirm')
         if password != password_check:
             raise ValidationError('Password does not match!', code='passwords_do_not_match')
+
+    def first_last_empty_check(self):
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+        if not first_name:
+            if not last_name:
+                raise ValidationError('Both first name and last name are empty!',
+                                      code='first_last_empty_values')
+
+    def clean(self):
+        super().clean()
+        self.password_check()
+        self.first_last_empty_check()
         return self.cleaned_data
+
+    def save(self, commit=True):
+        user=super(UserSignUpForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields=['username','password','password_confirm','first_name','last_name','email']
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,29 +1,27 @@
-import urllib
-from urllib.parse import urlencode
-
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 from django.contrib.auth.views import LogoutView
 from django.shortcuts import render, redirect
+from accounts.forms import UserSignUpForm
 
-from accounts.forms import SignUpForm
-
-
-def login_view(request, *args, **kwargs):
+def login_view(request):
     context={}
-
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        redirect_path = request.GET.get('next')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            redirect_path = request.GET.get('next')
-            if redirect_path == 'None' or redirect_path=='':
-                redirect_path = 'webapp:main_page'
-            return redirect(redirect_path)
+            if redirect_path:
+                return redirect(redirect_path)
+            return redirect('webapp:main_page')
         else:
             context['has_error']=True
+            context['next']=redirect_path
+            context['username']=username
+
+    else:
+        context={'next': request.GET.get('next')}
     return render(request, 'login.html', context=context)
 
 
@@ -31,16 +29,14 @@ class UserLogoutView(LogoutView):
     next_page = 'webapp:main_page'
 
 
-def register_view(request):
-    if request.method == 'GET':
-        form = SignUpForm()
-        return render(request, 'register.html', context={'form':form})
-    elif request.method == 'POST':
-        form = SignUpForm(data=request.POST)
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = UserSignUpForm(data=request.POST)
         if form.is_valid():
-            user = User(username=form.cleaned_data['username'])
-            user.set_password(form.cleaned_data['password'])
-            user.save()
+            user = form.save()
+            login(request,user)
             return redirect('webapp:main_page')
-        else:
-            return render(request, 'register.html', context={'form': form})
+    else:
+        form=UserSignUpForm()
+    return render(request, 'register.html', context={'form': form})
+
